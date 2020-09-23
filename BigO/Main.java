e2472c4996e7fb35ed2c2c8367e08d159520454d
himanshu shinde <Himanshu.shinde2u@gmail.com>
2020-09-23 05:18:38 +0000

    BigO first commmit....

---
+++ BigO/Main.java
@@ -0,0 +1,13 @@
+package BigO;
+class Main {
+  
+  private static String[] name = new String[]{"himanshu"};
+  private static void printArray(String[] name){
+    for(int i=0; i < name.length ; i++){ 
+      System.out.println(name[i]);
+    }
+  }
+  public static void main(String[] args) {
+    printArray(name);
+  }
+}
